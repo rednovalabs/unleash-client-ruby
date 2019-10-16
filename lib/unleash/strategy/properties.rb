c69a95ec5930d20783e1ea04d2b3d254b1bc29f8
Eric Hankins <ehankins@rednovalabs.com>
2019-10-16 12:50:02 -0500

    Add generic properties strategy

---
+++ lib/unleash/strategy/properties.rb
@@ -0,0 +1,24 @@
+module Unleash
+  module Strategy
+    class Properties < Base
+      PROPERTY_PARAM = 'property'
+      VALUE_PARAM = 'value'
+
+      def name
+        'properties'
+      end
+
+      def is_enabled?(params = {}, context = nil)
+        return false unless params.is_a?(Hash) && params.has_key?(PROPERTY_PARAM) && params.has_key?(VALUE_PARAM)
+        return false unless params.fetch(PROPERTY_PARAM, nil).is_a? String
+        return false unless params.fetch(VALUE_PARAM, nil).is_a? String
+        return false unless context.class.name == 'Unleash::Context'
+
+        possible_values = params[VALUE_PARAM].split(",").map(&:strip)
+        context_value = (context.properties[params[PROPERTY_PARAM]] || context.properties[params[PROPERTY_PARAM.to_sym]])&.to_s
+
+        possible_values.include? context_value
+      end
+    end
+  end
+end
